8dc96ddcf71b2fef260ea473e08423d13906f5cb
Yuuki K <yuuki3327@hotmail.com>
2016-04-10 21:31:40 -0700

    new js file added

--- common.js
+++ common.js
@@ -1,4 +1,38 @@
-document.getElementById('men').addEventListener('click', function MikataMen() {
+var idList = ['m1', 'm2', 'm3', 'm4', 'm5']; //array for id
+var EneidList = ['Em1', 'Em2', 'Em3', 'Em4', 'Em5']; //array for id
+var n = 0; //counter for id
+
+document.getElementById("men").addEventListener('click', function () {
     "use strict";
-    document.getElementById('m1').innerHTML = 'メ';
-});
+    if (document.getElementById(idList[n]).innerHTML.length < 2) {
+        document.getElementById(idList[n]).innerHTML += 'メ';
+    } else {alert("次へを押してください"); }
+}, false);
+
+document.getElementById("ko").addEventListener('click', function () {
+    "use strict";
+    if (document.getElementById(idList[n]).innerHTML.length < 2) {
+        document.getElementById(idList[n]).innerHTML += 'コ';
+    } else {alert("次へを押してください"); }
+}, false);
+
+document.getElementById("do").addEventListener('click', function () {
+    "use strict";
+    if (document.getElementById(idList[n]).innerHTML.length < 2) {
+        document.getElementById(idList[n]).innerHTML += 'ド';
+    } else {alert("次へを押してください"); }
+}, false);
+
+document.getElementById("tu").addEventListener('click', function () {
+    "use strict";
+    if (document.getElementById(idList[n]).innerHTML.length < 2) {
+        document.getElementById(idList[n]).innerHTML += 'ツ';
+    } else {alert("次へを押してください"); }
+}, false);
+
+    
+document.getElementById('next').addEventListener('click', function () {
+    "use strict";
+    if(n < 4){n++; }
+    else {alert("試合終了");}
+}, false);
